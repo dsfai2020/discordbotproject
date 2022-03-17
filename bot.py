f9f12e431e8f8a10a492f396c6326f5346056aa9
df-pc <ds.fai@outlook.com>
2022-03-16 17:43:09 -0700

    2.1

--- bot.py
+++ bot.py
@@ -27,10 +27,15 @@ async def on_message(message):
     
     trehistory= [
         "The Raven Elite was formed during the PS3 ERA on MAG.  The game had 300 active players in 150 vs 150 battles.  Each squad had 8 players and 1 Squad Leader.  4 Squads made up 1 platoon.  Each platoon had 1 Platoon Leader.  1 Company had 4 platoons.  Each Company had 1 OIC Officer in Charge."]
+
+    squad= ["Sniper Class -- Falcon 8, Snake Eyes"]
     
     if message.content == 'trehistory':
         response = random.choice(trehistory)
         await message.channel.send(response)
+
+    elif message.content == 'squad':
+        await message.channel.send(squad[0])
 
 # @client.event
 # async def on_message(message):
